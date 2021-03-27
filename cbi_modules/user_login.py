d357280b8d97012591f190eb9e706572fb309d50
cbi20-Autuk0901 <75194239+cbi20-Autuk0901@users.noreply.github.com>
2021-03-27 21:33:24 +0530

    Add files via upload

--- cbi_modules/user_login.py
+++ cbi_modules/user_login.py
@@ -1,30 +1,26 @@
 import psycopg2
 
-import json
-
-import os
-
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
 def login(data,psql):
 
-    user_email_address = data['user_email_address'].lower()
-    user_password = data['user_password']
+    user_email_address = data['userEmail'].lower()
+    user_password = data['userPassword']
 
     con = psycopg2.connect(database=psql['database'], user=psql['user'],password=psql['password'], host=psql['host'], port=psql['port'])
 
     cur = con.cursor()
 
-    cur.execute(f"SELECT user_first_name,user_last_name,user_password from CBI_User  WHERE user_email_address='{user_email_address}'")
+    cur.execute(f"SELECT user_first_name,user_last_name,user_password,user_category from CBI_User  WHERE user_email_address='{user_email_address}'")
     user = cur.fetchone()
 
     con.close()
 
     if user is None:
-            return {"error": "Invalid Email Address"}
+            return {"error": "Invalid Email Address"},401
     else:
         if check_password_hash(user[2], user_password):
-            return {'user_first_name': user[0], 'user_last_name': user[1]}
+            return {'firstName': user[0], 'lastName': user[1], 'userEmail': user_email_address, 'userRole': user[3]}, 200
         else:
-            return {"error": " Invalid Password"}
+            return {"error": " Invalid Password"},403
--- cbi_modules/user_register.py
+++ cbi_modules/user_register.py
@@ -1,26 +1,58 @@
 import psycopg2
 
-import json
-
-import os
-
 from werkzeug.security import generate_password_hash, check_password_hash
 
 def register(data,psql):
 
     try:
-        user_first_name = data['user_first_name']
-        user_last_name = data['user_last_name']
-        user_company = data['user_company']
-        user_email_address = data['user_email_address'].lower()
-        user_password = data['user_password']
-        user_category = data['user_category']
-        user_location = data['user_location']
-        invoice_company_name = data['invoice_company_name']
-        invoice_registration_number = data['invoice_registration_number']
-        invoice_billing_address = data['invoice_billing_address']
-        invoice_email_address = data['invoice_email_address']
-        invoice_phone_number = data['invoice_phone_number']
+        user_first_name = data['firstName']
+        if user_first_name is "":
+            user_first_name = None
+
+        user_last_name = data['lastName']
+        if user_last_name is "":
+            user_last_name = None
+
+        user_company = data['companyName']
+        if user_company is "":
+            user_company = None
+
+        user_email_address = data['userEmail'].lower()
+        if user_email_address is "":
+            user_email_address = None
+
+        user_password = data['password']
+        if user_password is "":
+            user_password = None
+
+        user_category = data['userRole']
+        if user_category is "":
+            user_category = None
+
+        user_location = data['location']
+        if user_location is "":
+            user_location = None
+
+        invoice_company_name = data['invoiceCompanyName']
+        if invoice_company_name is "":
+            invoice_company_name = None
+
+        invoice_registration_number = data['businessRegistrationNo']
+        if invoice_registration_number is "":
+            invoice_registration_number = None
+
+        invoice_billing_address = data['businessAddress']
+        if invoice_billing_address is "":
+            invoice_billing_address = None
+
+        invoice_email_address = data['invoiceEmail']
+        if invoice_email_address is "":
+            invoice_email_address = None
+
+        invoice_phone_number = data['phoneNumber']
+        if invoice_phone_number is "":
+            invoice_phone_number = None
+
 
         con = psycopg2.connect(database=psql['database'], user=psql['user'],password=psql['password'], host=psql['host'], port=psql['port'])
 
@@ -34,7 +66,7 @@ def register(data,psql):
         con.commit()
         con.close()
 
-        return {'status': 200}
+        return {'userEmail': user_email_address}, 200
 
     except Exception as e:
         error= str(e)
@@ -45,4 +77,4 @@ def register(data,psql):
         else:
             msg = error
         
-        return {'error': msg}
+        return {'error': msg},409
